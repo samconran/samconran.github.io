6439d8f29d305dc5876fe41dbc3e11cc482cd998
Sam <samconran7@gmail.com>
2019-01-13 13:41:36 +0000

    Updated hub page

--- hub/hub.js
+++ hub/hub.js
@@ -1,1 +1,6 @@
+let projects = document.getElementsByClassName('project-div');
 
+for (let p of projects)
+    p.addEventListener('click', function() {
+        window.open(p.attributes.href.value);
+    });
