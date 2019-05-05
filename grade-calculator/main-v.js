a3109bab7537f2373b0b86b089fb22fbfabc1013
samconran <samconran7@gmail.com>
2019-05-05 14:06:07 +0100

    Adds initial working files

---
+++ grade-calculator/main-v.js
@@ -0,0 +1,29 @@
+const AMGC = new Vue({
+  data : {
+    
+  }
+});
+
+Vue.component('displayModules', {
+  data() {
+    return {
+      key: value
+    }
+  },
+});
+
+Vue.component('setModules', {
+  data() {
+    return {
+      key: value
+    }
+  },
+})
+
+Vue.component('results', {
+  data() {
+    return {
+      key: value
+    }
+  },
+});
