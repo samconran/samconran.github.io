abb62798aaa67eee3359dae6b8745dd27956f09e
samconran <samconran7@gmail.com>
2019-05-05 15:26:02 +0100

    Adds mutation logic for results

--- grade-calculator/main.js
+++ grade-calculator/main.js
@@ -4,8 +4,44 @@ const store = new Vuex.Store({
       new Module('example', [40]),
       new Module('another', [])
     ]
+  },
+  mutations: {
+    addModule (state, module) {
+      state.modules = state.modules.push(module);
+      calculateResults(module.id);
+    },
+    calculateResults(state, moduleId)
+    {
+      let module = state.modules.find(obj => obj.id === moduleId),
+          currentMarks = module.inputMarks,
+          getScore = c => (c.score/100 * c.worth),
+          currentScore = currentMarks.reduce((a, b) => (getScore(a) + getScore(b))),
+          currentWorth = currentMarks.reduce((a,b) => (a.worth + b.worth)),
+          getResult = (n) => Math.ceil((n - currentScore) / (100-currentWorth) * 100);
+      
+      module.requiredMarks = {
+        'Pass' : getResult(40),
+        '2:2' : getResult(50),
+        '2:1' : getResult(60),
+        'First' : getResult(70)
+      }    
+    }
   }
-})
+});
+
+/*
+{
+  name : "example",
+  id: "example",
+  inputMarks: [
+    {
+      name : "test1"
+      score : 55
+      worth : 50
+    }
+  ]
+}
+*/
 
 const AMGC = new Vue({
   store,
@@ -34,6 +70,7 @@ function Module (name, marks) {
   this.name = name;
   this.id = truncate(name);
   this.current = marks || [];
+  this.needed = {};
   function truncate(s) {
     return (s.toLowerCase().replace(/\s+/g, '-'));
   }
