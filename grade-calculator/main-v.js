806937ce7c4a69235b18f18d1a4590d9a9ed550d
samconran <samconran7@gmail.com>
2019-05-05 14:23:15 +0100

    Adds VueX template

--- grade-calculator/main-v.js
+++ grade-calculator/main-v.js
@@ -1,29 +1,40 @@
+const store = new Vuex.Store({
+  state: {
+    modules : [
+      new Module('example', [40]),
+      new Module('another', [])
+    ]
+  }
+})
+
 const AMGC = new Vue({
+  store,
   data : {
 
   }
 });
 
 Vue.component('DisplayModules', {
-  data() {
-    return {
-      key: value
+  computed: {
+    modules () {
+      return this.$store.state.modules
     }
   },
 });
-
-Vue.component('SetModules', {
-  data() {
-    return {
-      key: value
-    }
-  },
-})
 
 Vue.component('Results', {
-  data() {
-    return {
-      key: value
+  computed: {
+    modules () {
+      return this.$store.state.modules
     }
   },
 });
+
+function Module (name, marks) {
+  this.name = name;
+  this.id = truncate(name);
+  this.current = marks || [];
+  function truncate(s) {
+    return (s.toLowerCase().replace(/\s+/g, '-'));
+  }
+}
